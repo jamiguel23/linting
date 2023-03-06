361b08231f9666655664350d65295d30bf386dd6
Matt Miguel <josematthewmiguel@gmail.com>
2023-03-06 03:20:22 +0000

    added p tag

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-
+var data = require('./data/test.json');
 
 app.set('view engine', 'ejs');
 
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+
 app.get('/', (req, res) =>{
   var title = 'home page';
-  res.render('index',{'title' : title});
-  // res.status(200).send('Ola world')
+  res.render('pages/index',{'title' : title});
+});
+
+app.get('/about', (req, res) =>{
+  var title = 'about page';
+  res.render('pages/about',{'title' : title});
+});
+
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
+});
+
+
+app.get('/projects', (req, res) =>{
+  var title = 'projects';
+  res.render('pages/projects',{'title' : title});
+});
+
+app.get('/contact', (req, res) =>{
+  var title = 'contact';
+  res.render('pages/contact',{'title' : title});
+});
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+ res.render('users/view', {
+     title: title,
+     user: data[--id]
+ });
 });
 
 app.listen(PORT, () => {
